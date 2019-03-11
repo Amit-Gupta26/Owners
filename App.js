9f8554d31ca6504d2552ce2ea2d921274c7af4f1
Amit Gupta <amithigh.gupta@gmail.com>
2019-03-11 19:20:52 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import { StyleSheet, View} from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends Component {
+  render() {
+    return (
+    <View style={styles.container}>
+     <MapView
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
