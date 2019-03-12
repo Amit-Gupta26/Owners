8563b1a1e7ff9295697c9eaefd03c87722b65872
Amit Gupta <amithigh.gupta@gmail.com>
2019-03-12 15:09:42 +0530

    Added Linking for geocoder packages.

--- App.js
+++ App.js
@@ -11,8 +11,24 @@ import React, {Component} from 'react';
 import { StyleSheet, View} from 'react-native';
 import MapView from 'react-native-maps';
 import Search from './extras/Search';
+import Geocoder from 'react-native-geocoder';
 
 export default class App extends Component {
+
+  componentDidMount(){
+    var NY = {
+      lat: 40.7809261,
+      lng: -73.9637594
+    };
+    
+    Geocoder.geocodePosition(NY).then(res => {
+      let code = res[0].adminArea;
+      alert(code);
+        // res is an Array of geocoding object (see below)
+    })
+    .catch(err => console.warn(err))
+
+  }
   render() {
     return (
     <View style={styles.container}>
--- android/app/src/main/java/com/maps/MainApplication.java
+++ android/app/src/main/java/com/maps/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,6 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
           return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+              new RNGeocoderPackage(),
               new MapsPackage()
           );
       }
